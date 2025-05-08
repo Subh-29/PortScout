e5ffb22b3126814c2bedd5105f5491edf0843939
Subh-29 <subhradipkar101@gmail.com>
2025-05-08 17:30:23 +0530

    Created portScanner.py

---
+++ portScanner.py
@@ -0,0 +1,21 @@
+import socket
+
+from concurrent.futures import ThreadPoolExecutor as TPE
+
+target = input("Enter target IP or Domain Name: ")
+ports = 30
+
+def scanPort(port, target):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(0.5)
+        result = s.connect_ex((target, port))
+
+        if result == 0:
+            print(f"[+] Port {port} is OPEN")
+        else:
+            print(f"[-] Port {port} is CLOSED!")
+        s.close()
+    except KeyboardInterrupt:
+        pass
+
